1437a145893c6d1ca1a0c55fa87a0ac765fe4d9d
Peter B Yorke <pbyorke@gmail.com>
2023-06-03 21:56:16 -0700

    Initial Commit

---
+++ HouseOfYorke/HouseOfYorkeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseOfYorkeApp.swift
+//  HouseOfYorke
+//
+//  Created by Peter Yorke on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HouseOfYorkeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
